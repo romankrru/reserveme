6b21dfdf5368ea95d670a871e18451deb4e79f01
romankrru <rm07ru@gmail.com>
2018-09-15 16:20:47 +0300

    Add basic gql api

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const os = require('os');
+const path = require('path');
 const {ApolloServer, gql} = require('apollo-server-express');
 const cors = require('cors');
 
@@ -8,19 +9,35 @@ app.use(cors());
 
 const schema = gql`
 	type Query {
+		users: [User!]
 		me: User
+		user(id: ID!): User
 	}
 
 	type User {
+		id: ID!
 		username: String!
 	}
 `;
 
+const users = {
+	1: {
+		id: '1',
+		username: 'Robin Wieruch',
+	},
+	2: {
+		id: '2',
+		username: 'Dave Davids',
+	},
+};
+
+const me = users[1];
+
 const resolvers = {
 	Query: {
-		me: () => ({
-			username: 'Roman',
-		}),
+		me: () => me,
+		user: (parent, args) => users[args.id],
+		users: () => Object.values(users),
 	},
 };
 
