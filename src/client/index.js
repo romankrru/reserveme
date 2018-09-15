5e7dd595a7517cced0007411d662f36a84c8fcf4
romankrru <rm07ru@gmail.com>
2018-09-15 16:20:26 +0300

    Move apollo client setup to index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ApolloClient} from 'apollo-client';
+import {ApolloProvider} from 'react-apollo';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+import {onError} from 'apollo-link-error';
+import {ApolloLink} from 'apollo-link';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+	cache: new InMemoryCache(),
+
+	link: ApolloLink.from([
+		onError(({graphQLErrors, networkError}) => {
+			if (graphQLErrors)
+				graphQLErrors.map(({message, locations, path}) =>
+					console.log(
+						`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+					)
+				);
+
+			if (networkError) console.log(`[Network error]: ${networkError}`);
+		}),
+
+		new HttpLink({
+			credentials: 'same-origin',
+			uri: 'http://localhost:8080/graphql',
+		}),
+	]),
+});
+
+const app = (
+	<ApolloProvider client={client}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</ApolloProvider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
