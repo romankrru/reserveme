ebbb340a72434e381af3874d1d295c542dfa8f58
romankrru <rm07ru@gmail.com>
2018-09-15 21:54:59 +0300

    ---

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const os = require('os');
-const path = require('path');
+const uuidv4 = require('uuid/v4');
 const {ApolloServer, gql} = require('apollo-server-express');
 const cors = require('cors');
 
@@ -12,36 +12,93 @@ const schema = gql`
 		users: [User!]
 		me: User
 		user(id: ID!): User
+		messages: [Message!]!
+		message(id: ID!): Message!
 	}
 
 	type User {
 		id: ID!
+		messages: [Message!]
 		username: String!
+	}
+
+	type Message {
+		id: ID!
+		text: String!
+		user: User!
+	}
+
+	type Mutation {
+		createMessage(text: String!): Message!
 	}
 `;
 
 const users = {
 	1: {
 		id: '1',
+		messageIds: [1],
 		username: 'Robin Wieruch',
 	},
 	2: {
 		id: '2',
+		messageIds: [2],
 		username: 'Dave Davids',
 	},
 };
 
-const me = users[1];
+const messages = {
+	1: {
+		id: '1',
+		text: 'Hello World',
+		userId: '1',
+	},
+	2: {
+		id: '2',
+		text: 'By World',
+		userId: '2',
+	},
+};
 
 const resolvers = {
+	Message: {
+		user: message => users[message.userId],
+	},
+
+	Mutation: {
+		createMessage: (parent, {text}, {me}) => {
+			const id = uuidv4();
+
+			const message = {
+				id,
+				text,
+				userId: me.id,
+			};
+
+			messages[id] = message;
+			users[me.id].messageIds.push(id);
+
+			return message;
+		},
+	},
+
 	Query: {
-		me: () => me,
+		me: (parent, args, {me}) => me,
+		message: (parent, {id}) => messages[id],
+		messages: () => Object.values(messages),
 		user: (parent, args) => users[args.id],
 		users: () => Object.values(users),
+	},
+
+	User: {
+		messages: user => Object.values(messages).filter(message => message.userId === user.id),
 	},
 };
 
 const server = new ApolloServer({
+	context: {
+		me: users[1],
+	},
+
 	resolvers,
 	typeDefs: schema,
 });
