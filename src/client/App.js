81b2e2d03a2f21f861e8878485e7ddbda915cb24
romankrru <rm07ru@gmail.com>
2018-09-15 14:07:47 +0300

    Set up apollo

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,72 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {ApolloClient} from 'apollo-client';
+import {ApolloProvider, Query} from 'react-apollo';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+import {onError} from 'apollo-link-error';
+import {ApolloLink} from 'apollo-link';
+import gql from 'graphql-tag';
 import './app.css';
-import ReactImage from './react.png';
 
+const client = new ApolloClient({
+	cache: new InMemoryCache(),
+
+	link: ApolloLink.from([
+		onError(({graphQLErrors, networkError}) => {
+			if (graphQLErrors)
+				graphQLErrors.map(({message, locations, path}) =>
+					console.log(
+						`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+					)
+				);
+
+			if (networkError) console.log(`[Network error]: ${networkError}`);
+		}),
+
+		new HttpLink({
+			credentials: 'same-origin',
+			uri: 'http://localhost:8080/graphql',
+		}),
+	]),
+});
+
+const q = gql`
+	{
+		me {
+			username
+		}
+	}
+`;
 export default class App extends Component {
-  state = { username: null };
+	state = {username: null};
 
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+	componentDidMount() {
+		fetch('/api/getUsername')
+			.then(res => res.json())
+			.then(user => this.setState({username: user.username}));
+	}
 
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<ApolloProvider client={client}>
+				<Query query={q}>
+					{data => {
+						if (data.loading) {
+							return <p>loading...</p>;
+						}
+
+						return (
+							<div>
+								<p>
+									Hello,
+									{data.data.me.username}
+								</p>
+								{this.state.username}
+							</div>
+						);
+					}}
+				</Query>
+			</ApolloProvider>
+		);
+	}
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const os = require('os');
+const {ApolloServer, gql} = require('apollo-server-express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+
+const schema = gql`
+	type Query {
+		me: User
+	}
+
+	type User {
+		username: String!
+	}
+`;
+
+const resolvers = {
+	Query: {
+		me: () => ({
+			username: 'Roman',
+		}),
+	},
+};
+
+const server = new ApolloServer({
+	resolvers,
+	typeDefs: schema,
+});
+
+server.applyMiddleware({app, path: '/graphql'});
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.get('/api/getUsername', (req, res) => res.send({username: os.userInfo().username}));
 app.listen(8080, () => console.log('Listening on port 8080!'));
