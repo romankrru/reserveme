c2b8023c274901272aa267354a8cabad24704822
romankrru <rm07ru@gmail.com>
2018-09-16 03:11:46 +0300

    Setup auth form with formik

--- src/client/App/Auth/Form/index.js
+++ src/client/App/Auth/Form/index.js
@@ -1,21 +1,40 @@
 import React from 'react';
+import _ from 'lodash';
 import {Button, Form} from 'semantic-ui-react';
+import {withFormik} from 'formik';
+import Yup from 'yup';
+import Input from '../../generic/Input';
 
-const FormExampleForm = () => (
-	<Form>
-		<Form.Field>
-			<label htmlFor="email">
-				Email
-				<input id="email" placeholder="Email" />
-			</label>
-		</Form.Field>
+const FormExampleForm = props => (
+	<Form onSubmit={props.handleSubmit}>
+		<Input
+			label="Email"
+			id="email"
+			name="email"
+			placeholder="Email"
+			isErrorShown={props.errors.email && props.touched.email}
+			value={props.values.email}
+			onChange={props.handleChange}
+			errorMessage={{
+				content: _.capitalize(props.errors.email),
+				header: 'Invalid email',
+			}}
+		/>
 
-		<Form.Field>
-			<label htmlFor="password">
-				Password
-				<input id="password" type="password" placeholder="Password" />
-			</label>
-		</Form.Field>
+		<Input
+			label="Password"
+			id="password"
+			name="password"
+			placeholder="Password"
+			type="password"
+			isErrorShown={props.errors.password && props.touched.password}
+			value={props.values.password}
+			onChange={props.handleChange}
+			errorMessage={{
+				content: _.capitalize(props.errors.password),
+				header: 'Invalid password',
+			}}
+		/>
 
 		<Button fluid type="submit">
 			Submit
@@ -23,4 +42,21 @@ const FormExampleForm = () => (
 	</Form>
 );
 
-export default FormExampleForm;
+export default withFormik({
+	handleSubmit: values => console.log(values),
+
+	mapPropsToValues: () => ({
+		email: '',
+		password: '',
+	}),
+
+	validationSchema: Yup.object().shape({
+		email: Yup.string()
+			.email()
+			.required(),
+
+		password: Yup.string()
+			.min(8)
+			.required(),
+	}),
+})(FormExampleForm);
