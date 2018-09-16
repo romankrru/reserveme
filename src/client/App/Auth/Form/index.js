180e4cbefbeedc03f82477bfec46eb531834175b
romankrru <rm07ru@gmail.com>
2018-09-16 03:26:34 +0300

    Add Login, SignUp messages

--- src/client/App/Auth/Form/index.js
+++ src/client/App/Auth/Form/index.js
@@ -1,45 +1,47 @@
 import React from 'react';
 import _ from 'lodash';
-import {Button, Form} from 'semantic-ui-react';
+import {Button, Form, Segment} from 'semantic-ui-react';
 import {withFormik} from 'formik';
 import Yup from 'yup';
 import Input from '../../generic/Input';
 
 const FormExampleForm = props => (
-	<Form onSubmit={props.handleSubmit}>
-		<Input
-			label="Email"
-			id="email"
-			name="email"
-			placeholder="Email"
-			isErrorShown={props.errors.email && props.touched.email}
-			value={props.values.email}
-			onChange={props.handleChange}
-			errorMessage={{
-				content: _.capitalize(props.errors.email),
-				header: 'Invalid email',
-			}}
-		/>
+	<Segment>
+		<Form onSubmit={props.handleSubmit}>
+			<Input
+				label="Email"
+				id="email"
+				name="email"
+				placeholder="Email"
+				isErrorShown={props.errors.email && props.touched.email}
+				value={props.values.email}
+				onChange={props.handleChange}
+				errorMessage={{
+					content: _.capitalize(props.errors.email),
+					header: 'Invalid email',
+				}}
+			/>
 
-		<Input
-			label="Password"
-			id="password"
-			name="password"
-			placeholder="Password"
-			type="password"
-			isErrorShown={props.errors.password && props.touched.password}
-			value={props.values.password}
-			onChange={props.handleChange}
-			errorMessage={{
-				content: _.capitalize(props.errors.password),
-				header: 'Invalid password',
-			}}
-		/>
+			<Input
+				label="Password"
+				id="password"
+				name="password"
+				placeholder="Password"
+				type="password"
+				isErrorShown={props.errors.password && props.touched.password}
+				value={props.values.password}
+				onChange={props.handleChange}
+				errorMessage={{
+					content: _.capitalize(props.errors.password),
+					header: 'Invalid password',
+				}}
+			/>
 
-		<Button fluid type="submit">
-			Submit
-		</Button>
-	</Form>
+			<Button fluid type="submit">
+				Submit
+			</Button>
+		</Form>
+	</Segment>
 );
 
 export default withFormik({
--- src/client/App/Auth/index.js
+++ src/client/App/Auth/index.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {withPropsOnChange, compose} from 'recompose';
+import {Message, Icon} from 'semantic-ui-react';
 import Form from './Form';
 import styles from './assets/styles.css';
+
+const LoginMessage = () => (
+	<Message attached="bottom" warning>
+		<Icon name="help" />
+		Dont&apos;t have an account?&nbsp;
+		<Link to="/auth/register">Sign up here.</Link>
+	</Message>
+);
+
+const SignupMessage = () => (
+	<Message attached="bottom" warning>
+		<Icon name="help" />
+		Already signed up?&nbsp;
+		<Link to="/auth">Switch to Login.</Link>
+	</Message>
+);
 
 const Auth = props => (
 	<div className={styles.auth}>
 		<h2>{props.isLogin ? 'Login' : 'Register'}</h2>
 		<Form />
-
-		<h5>
-			{props.isLogin ? (
-				<Link to="/auth/register">Switch to Register</Link>
-			) : (
-				<Link to="/auth">Switch to Login</Link>
-			)}
-		</h5>
+		{props.isLogin ? <LoginMessage /> : <SignupMessage />}
 	</div>
 );
 
