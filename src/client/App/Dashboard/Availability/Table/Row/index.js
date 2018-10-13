08e1ef74a18821f66d527d6479ab97149976e52f
romankrru <rm07ru@gmail.com>
2018-10-13 21:53:05 +0300

    Add types for row

--- src/client/App/Dashboard/Availability/Table/Row/index.js
+++ src/client/App/Dashboard/Availability/Table/Row/index.js
@@ -1,10 +1,19 @@
+// @flow
 import React from 'react';
 import {Checkbox, Table} from 'semantic-ui-react';
 import classNames from 'classnames';
 import styles from './asstes/styles.css';
 import EditableTimeInput from './EditableTimeInput';
 
-const Row = props => (
+const Row = (props: {
+	isEditing: boolean,
+
+	day: {
+		title: string,
+	},
+
+	startEditing: Function,
+}) => (
 	<Table.Row
 		className={classNames(styles.row, {
 			[styles.rowEditing]: props.isEditing,
